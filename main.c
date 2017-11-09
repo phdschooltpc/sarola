66d01ac3b02b357ac5f80c9a78f80d95ca4e4412
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2017-11-09 18:12:06 +0100

    cleanup

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #include "profiler.h"
 /*Intermittent Tester*/
 #include <tester.h>
-//#include <noise.h>
 
 /*
  *******************************************************************************
@@ -35,12 +34,6 @@ NewTask(TASK_FANN_TEST, task_fann_test, 1) // with self-field
 #pragma PERSISTENT(TASK_RESULT)
 NewTask(TASK_RESULT, task_result, 1) // with self-field
 
-//#pragma PERSISTENT(TASK_FIND_MIN)
-//NewTask(TASK_FIND_MIN, task_find_min_f, 1) // with self-field
-//
-//#pragma PERSISTENT(TASK_SUB_MIN)
-//NewTask(TASK_SUB_MIN, task_sub_min_f, 1) // with self-field
-
 /*
  *******************************************************************************
  * Inform the program about the task to execute on the first start of the
@@ -64,22 +57,6 @@ InitialTask(TASK_FANN_LOAD)
 /* task, name, type, len, code */
 NewSelfField(TASK_FANN_TEST, sf_test_index, UINT8, 1, SELF_FIELD_CODE_1)
 //
-//// This field connects the two tasks, informing TASK_SUB_MIN about the position
-//// of each array's minimum
-//#pragma PERSISTENT(PersField(TASK_FIND_MIN, TASK_SUB_MIN, f_min_index))
-//NewField(TASK_FIND_MIN, TASK_SUB_MIN, f_min_index, UINT8, NUM_OF_ARRAYS)
-//
-//// This self-field helps TASK_SUB_MIN keep track of the array whose average
-//// has to be computed
-//#pragma PERSISTENT(PersSField0(TASK_FIND_MIN, sf_array_index))
-//#pragma PERSISTENT(PersSField1(TASK_FIND_MIN, sf_array_index))
-//NewSelfField(TASK_SUB_MIN, sf_array_index, UINT8, 1, SELF_FIELD_CODE_1)
-//
-//// This self-field helps TASK_SUB_MIN keep track of each array's average,
-//// after an array has been subtracted its minimum
-//#pragma PERSISTENT(PersSField0(TASK_SUB_MIN, sf_avg))
-//#pragma PERSISTENT(PersSField1(TASK_SUB_MIN, sf_avg))
-//NewSelfField(TASK_SUB_MIN, sf_avg, FLOAT32, NUM_OF_ARRAYS, SELF_FIELD_CODE_2)
 
 /*
  *******************************************************************************
@@ -136,26 +113,9 @@ void task_fann_test(void) {
 
     uint8_t test_index;
     ReadSelfField_U8(TASK_FANN_TEST, sf_test_index, &test_index); // initially sf_array_index = 0
-/*
-    uint8_t i = num_data;
-    // num_data from thyroid_test.h: 100 -> uint8_t is sufficient
-    for (; i > 0; i--) {
-        fann_test(ann, input[i], output[i]);
-    }
-   */
-   //uint16_t i;
-   /* Debug variable. */
-    /*
-   fann_type *calc_out;
-   for (i = 0; i < num_data; i++) {
-           calc_out = fann_test(ann, input[i], output[i]);
-           __no_operation();
-   }
-   */
 
-    fann_test(ann, input[test_index], output[test_index]);
-
-    //puts("bar\n");
+    fann_type* calc_out = fann_test(ann, input[test_index], output[test_index]);
+    tester_send_data(test_index, calc_out, sizeof(calc_out));
 
 #ifdef PROFILE
     /* Stop counting clock cycles. */
@@ -199,108 +159,3 @@ void task_result(void) {
     /* You need to include that statement at the termination of your intermittent program*/
     //tester_send_data(0, string, 15);
 }
-
-//
-//uint8_t find_min_pos(uint16_t* array)
-//{
-//    uint8_t i, min_pos;
-//    uint16_t min = array[0];
-//
-//    min_pos = 0;
-//    for (i = 1; i < SIZE; i++) {
-//        if (array[i] < min) {
-//            min = array[i];
-//            min_pos = i;
-//        }
-//    }
-//
-//    return min_pos;
-//}
-//
-//
-//float sub_min_and_avg(uint16_t* array, uint8_t min_pos)
-//{
-//    uint8_t i;
-//    float avg = 0;
-//
-//    for (i = 0; i < SIZE; i++) {
-//        avg += array[i] - array[min_pos];
-//    }
-//
-//    return avg / SIZE;
-//}
-//
-//
-//void task_find_min_f()
-//{
-//    uint8_t array_index;
-//    uint8_t min_pos;
-//
-//    ReadSelfField_U8(TASK_FIND_MIN, sf_array_index, &array_index); // initially sf_array_index = 0
-//
-//    switch (array_index) {
-//    case 0:
-//        min_pos = find_min_pos(a);
-//        break;
-//    case 1:
-//        min_pos = find_min_pos(b);
-//        break;
-//    case 2:
-//        min_pos = find_min_pos(c);
-//        break;
-//    case 3:
-//        min_pos = find_min_pos(d);
-//        break;
-//    }
-//
-//    WriteFieldElement_U8(TASK_FIND_MIN, TASK_SUB_MIN, f_min_index, &min_pos, array_index);
-//
-//    if (++array_index == NUM_OF_ARRAYS) {
-//        array_index = 0;
-//        WriteSelfField_U8(TASK_FIND_MIN, sf_array_index, &array_index);
-//        StartTask(TASK_SUB_MIN);
-//    }
-//    else {
-//        WriteSelfField_U8(TASK_FIND_MIN, sf_array_index, &array_index);
-//        StartTask(TASK_FIND_MIN);
-//    }
-//}
-//
-//
-//void task_sub_min_f()
-//{
-//    uint8_t array_index;
-//    uint8_t min_pos[NUM_OF_ARRAYS];
-//    float avg[NUM_OF_ARRAYS];
-//
-//    ReadSelfField_U8(TASK_SUB_MIN, sf_array_index, &array_index); // initially sf_array_index = 0
-//    ReadField_U8(TASK_FIND_MIN, TASK_SUB_MIN, f_min_index, min_pos);
-//    ReadSelfField_F32(TASK_SUB_MIN, sf_avg, avg);
-//
-//    switch (array_index) {
-//    case 0:
-//        avg[array_index] = sub_min_and_avg(a, min_pos[array_index]);
-//        break;
-//    case 1:
-//        avg[array_index] = sub_min_and_avg(b, min_pos[array_index]);
-//        break;
-//    case 2:
-//        avg[array_index] = sub_min_and_avg(c, min_pos[array_index]);
-//        break;
-//    case 3:
-//        avg[array_index] = sub_min_and_avg(d, min_pos[array_index]);
-//        break;
-//    }
-//
-//    WriteSelfField_F32(TASK_SUB_MIN, sf_avg, avg);
-//
-//    if (++array_index == NUM_OF_ARRAYS) {
-//        array_index = 0;
-//        WriteSelfField_U8(TASK_SUB_MIN, sf_array_index, &array_index);
-//        while(1); // END OF PROGRAM, breakpoint here to check result
-//    }
-//    else {
-//        WriteSelfField_U8(TASK_SUB_MIN, sf_array_index, &array_index);
-//        StartTask(TASK_SUB_MIN);
-//    }
-//}
